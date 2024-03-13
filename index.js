96b19e055dab3749954c3790602f022c06ae7e59
Julie Zenou <me@juliezen.com>
2024-03-13 17:12:55 +1100

    set up main functionality for changing the images using random numbers and determining the winner

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1
+
+document.getElementById("img1").setAttribute("src", './images/dice'+ randomNumber1 + '.png');
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1
+
+document.getElementById("img2").setAttribute("src", './images/dice'+ randomNumber2 + '.png');
+
+if (randomNumber1 == randomNumber2) {
+    document.querySelector("h1").textContent = "Draw!"
+} else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "🚩 Player 1 Wins"
+} else {
+    document.querySelector("h1").textContent = "Player 2 Wins 🚩"
+}
